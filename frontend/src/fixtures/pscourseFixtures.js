2a109c51801aca83f5c49d52633d3f0d594839e4
Maria-Saucedo <msaucedo-flores@ucsb.edu>
2024-02-28 00:33:41 -0800

    Added personal schedule name, course name, and quarter to fixtures (this will likely cause tests to fail for now)

--- frontend/src/fixtures/pscourseFixtures.js
+++ frontend/src/fixtures/pscourseFixtures.js
@@ -4,17 +4,26 @@ const coursesFixtures = {
       id: 25,
       psId: 13,
       enrollCd: "08250",
+      psName: "PS One",
+      courseName: "CS111",
+      quarter: "W24"
     },
     {
       id: 26,
       psId: 13,
       enrollCd: "08276",
+      psName: "PS Two",
+      courseName: "CS154",
+      quarter: "W24"
     },
   ],
   oneCourse: {
     id: 27,
     psId: 13,
     enrollCd: "08078",
+    psName: "PS Single",
+    courseName: "CS156",
+    quarter: "W24"
   },
 };
 
